76f65a6795207b227c8414f0555c5dd3a90de705
Max Penet <mpenet@s-exp.com>
2025-02-07 16:56:20 +0100

    fixup! remove release.yml workflow

--- lein-eftest/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject lein-eftest "0.6.0"
-  :description "A fast and pretty Clojure test runner"
-  :url "https://github.com/weavejester/eftest"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
